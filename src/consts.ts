53f74a4b10c5483b8cfd53a21f10102a26a44841
Alexandre Papin <me@alexandrepapin.name>
2023-02-02 22:49:02 +0100

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Bits and baubles';
+export const SITE_DESCRIPTION = "Alexandre Papin's personal website";
